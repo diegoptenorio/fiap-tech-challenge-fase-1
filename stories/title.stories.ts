69bf65b42fa09fa11c05b9c5bf605153675ebb01
Diego Tenório <38768837+diegoptenorio@users.noreply.github.com>
2026-04-01 19:47:46 -0300

    task: Storybook setup

--- stories/title.stories.ts
+++ stories/title.stories.ts
@@ -3,7 +3,7 @@ import type { Meta, StoryObj } from "@storybook/nextjs-vite";
 import Title from "../app/components/title";
 
 const meta = {
-    title: "Example/Title",
+    title: "Components/Title",
     component: Title,
     parameters: {
         type: "",
