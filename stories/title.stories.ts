22392409c7897280f321cc47d15d4a651df9f7da
Diego Tenório <38768837+diegoptenorio@users.noreply.github.com>
2026-04-01 19:02:40 -0300

    task: Storybook setup

---
+++ stories/title.stories.ts
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from "@storybook/nextjs-vite";
+
+import Title from "../app/components/title";
+
+const meta = {
+    title: "Example/Title",
+    component: Title,
+    parameters: {
+        type: "",
+    },
+    tags: ["autodocs"],
+} satisfies Meta<typeof Title>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+    args: {
+        type: "h1",
+        children: "This is a Title",
+    },
+};
